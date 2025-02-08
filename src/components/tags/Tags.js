8d0a0f3545174d9dcbb3c25015eb2ec24599d852
Phanindra <phanindragadireddy2@gmail.com>
2025-02-08 16:16:50 +0530

    filteres added

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Tags.css'
 
 function Tags() {
-    const tags = ['All', 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Sci-Fic' ,'Kids', 'Thriller', 'War', 'Western']
+    const tags = ['All', 'Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance','Kids', 'Thriller', 'War', 'Western']
   return (
     <>
         <div className="tags">
