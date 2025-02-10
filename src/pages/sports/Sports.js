52d4e9096066f782c8366112d9b38938afc6a42d
Phanindra <phanindragadireddy2@gmail.com>
2025-02-10 14:44:53 +0530

    movie,sports and tv shows section added

--- src/pages/sports/Sports.js
+++ src/pages/sports/Sports.js
@@ -13,6 +13,10 @@ function Sports() {
 
   const [sports, setSports] = useState([])
   const [highlight, setHighlight] = useState([])
+  const [bestMemories, setBestMemories] = useState([])
+  const [kohli, setKohli] = useState([])
+  const [ipl, setIpl] = useState([])
+  const [bhuvi, setBhuvi] = useState([])
 
 
   const fetchData = async () => {
@@ -29,11 +33,30 @@ function Sports() {
 
       let highlight = data.filter(movie => { return movie.genre.includes('Cricket') })
       console.log(highlight)
-      setHighlight(highlight)
+      setHighlight(highlight.slice(0, 10))
+
+      let bestMemories = data.filter(movie => { return movie.cricket === true })
+      console.log(bestMemories)
+      setBestMemories(bestMemories.slice(0, 10))
+
+      let kohli = data.filter(movie => { return movie.kohli === true })
+      console.log(kohli)
+      setKohli(kohli.slice(0, 20))
+
+      let ipl = data.filter(movie => { return movie.ipl === true })
+      console.log(ipl)
+      setIpl(ipl.slice(0, 20))
+
+      let bhuvi = data.filter(movie => { return movie.bhuneswar === true })
+      console.log(bhuvi)
+      setBhuvi(bhuvi.slice(0, 20))
+
+
     }
     catch (error) {
       console.log(error)
     }
+
 
   }
 
@@ -49,6 +72,10 @@ function Sports() {
       <div className={styles.content}>
         <Shows title="Sports" movies={sports} />
         <Shows title="Cricket Highlights" movies={highlight} />
+        <Shows title="Best Moments of india in 2024" movies={bestMemories} />
+        <Shows title="Kohli's Best Moments" movies={kohli} />
+        <Shows title="IPL 2024 moments" movies={ipl} />
+        <Shows title="Bhuneswar's Best Moments" movies={bhuvi} />
       </div>
     </>
   )
--- src/pages/tvShows/TvShows.js
+++ src/pages/tvShows/TvShows.js
@@ -12,6 +12,11 @@ function TvShows() {
   //filtering the movies
 
   const [tvShows, setTvShows] = useState([])
+  const [tvShows2, setTvShows2] = useState([])
+  const [tvShows3, setTvShows3] = useState([])
+  const [tvShows4, setTvShows4] = useState([])
+  const [tvShows5, setTvShows5] = useState([])
+  const [tvShows6, setTvShows6] = useState([])
 
   const fetchData = async () => {
     try {
@@ -23,11 +28,34 @@ function TvShows() {
 
       let tvShows = data.filter(movie => { return movie.genre.includes('TV Series') })
       console.log(tvShows)
-      setTvShows(tvShows)
+      setTvShows(tvShows.slice(0, 20))
+
+      let tvShows2 = data.filter(movie => { return movie.genre.includes('TV Series', 'Drama') })
+      console.log(tvShows2)
+      setTvShows2(tvShows2.slice(20, 40))
+
+      let tvShows3 = data.filter(movie => { return movie.genre.includes('Comedy') })
+      console.log(tvShows3)
+      setTvShows3(tvShows3.slice(0, 20))
+
+      let tvShows4 = data.filter(movie => { return movie.genre.includes( 'Action') })
+      console.log(tvShows4)
+      setTvShows4(tvShows4.slice(0,20))
+
+      let tvShows5 = data.filter(movie => { return movie.genre.includes( 'Crime') })
+      console.log(tvShows5)
+      setTvShows5(tvShows5.slice(0, 20))
+
+      let tvShows6 = data.filter(movie => { return movie.genre.includes('Adventure') })
+      console.log(tvShows6)
+      setTvShows6(tvShows6.slice(0, 20))
     }
     catch (error) {
       console.log(error)
     }
+
+
+
   }
 
   useEffect(() => {
@@ -41,7 +69,13 @@ function TvShows() {
       <Carousel />
       <div className='content1'>
         <Shows title="Tv Shows" movies={tvShows} />
+        <Shows title="Drama" movies={tvShows2} />
+        <Shows title="Comedy" movies={tvShows3} />  
+        <Shows title="Action" movies={tvShows4} />
+        <Shows title="Crime" movies={tvShows5} />
+        <Shows title="Adventure" movies={tvShows6} />
       </div>
+
 
     </>
 
